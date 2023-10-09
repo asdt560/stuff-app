b24d47320ea194109926aaec8aebf1817aa8152b
JustoRago <103001263+asdt560@users.noreply.github.com>
2023-10-09 16:54:03 -0300

    Delete generated components

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -8,13 +8,4 @@
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
-}
 </style>
