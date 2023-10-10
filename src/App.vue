e631b9cc9a0c74d861c6e21cf9eff920992b1c46
JustoRago <103001263+asdt560@users.noreply.github.com>
2023-10-10 16:35:08 -0300

    Fix up app display, remove old styles

--- src/App.vue
+++ src/App.vue
@@ -16,4 +16,28 @@ import { RouterLink, RouterView } from 'vue-router'
 </template>
 
 <style scoped>
+header {
+  height: 2rem;
+  display: flex;
+  align-items: center;
+}
+
+nav {
+    display: flex;
+    gap: 1rem;
+}
+
+a {
+  all: unset;
+  font-size: 1.25rem;
+  color: green;
+  cursor: pointer;
+  padding: 0.5rem
+}
+
+a:hover {
+  color: #fff;
+  background: green;
+}
+
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
+import { useCounterStore } from '../stores/counter'
+
+const counter = useCounterStore()
 </script>
 
 <template>
   <main>
+    <p>{{counter.count}}</p>
+    <button @click="counter.increment">Increment</button>
+    <button @click="counter.decrement">Decrement</button>
   </main>
 </template>
