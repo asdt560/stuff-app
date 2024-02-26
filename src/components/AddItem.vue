9a3bda325e2426cbd015ce42f0e873d6c8a79177
JustoRago <103001263+asdt560@users.noreply.github.com>
2024-02-26 20:37:09 -0300

    Add basic menu readability

--- src/components/AddItem.vue
+++ src/components/AddItem.vue
@@ -19,9 +19,9 @@
 </script>
 
 <template>
-  <form class="flex" @submit.prevent="handleSubmit">
-    <input type="text" v-model="newItem.name" placeholder="Item Name" required>
-    <textarea v-model="newItem.description" placeholder="Item Description" required></textarea>
-    <button type="submit">Add</button>
+  <form class="flex flex-col" @submit.prevent="handleSubmit">
+    <input class="border-2" type="text" v-model="newItem.name" placeholder="Item Name" required>
+    <textarea class="border-2" v-model="newItem.description" placeholder="Item Description" required></textarea>
+    <button class="border-2" type="submit">Add</button>
   </form>
 </template>
