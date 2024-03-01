db210e54e9ecc8f645a91e277dc0095697332fa6
JustoRago <103001263+asdt560@users.noreply.github.com>
2024-02-29 21:18:54 -0300

    Add form style

--- src/components/AddItem.vue
+++ src/components/AddItem.vue
@@ -19,9 +19,9 @@
 </script>
 
 <template>
-  <form class="flex flex-col" @submit.prevent="handleSubmit">
-    <input class="border-2" type="text" v-model="newItem.name" placeholder="Item Name" required>
-    <textarea class="border-2" v-model="newItem.description" placeholder="Item Description" required></textarea>
-    <button class="border-2" type="submit">Add</button>
+  <form class="flex flex-col gap-4" @submit.prevent="handleSubmit">
+    <input class="border-2 p-2 border-green-500" type="text" v-model="newItem.name" placeholder="Item Name" required>
+    <textarea class="border-2 p-2 border-green-500" v-model="newItem.description" placeholder="Item Description" required></textarea>
+    <button class="border-2 p-2 border-green-500" type="submit">Add</button>
   </form>
 </template>
